11e9fee352eee0bb619a3432ae91c9e87038ab90
Nathanial L. McConnell <fluxaugur@gmail.com>
2012-07-09 06:05:05 -0400

    Initial Commit

---
+++ Proximity2/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  Proximity2
+//
+//  Created by Nathanial McConnell on 7/9/12.
+//  Copyright (c) 2012 East Tennessee State University. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ Proximity2/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Proximity2
+//
+//  Created by Nathanial McConnell on 7/9/12.
+//  Copyright (c) 2012 East Tennessee State University. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+  return NSApplicationMain(argc, (const char **)argv);
+}
