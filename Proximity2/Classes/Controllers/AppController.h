27d2c1a6e9db47f375ce4ad3f5b0dd597bc4fbb4
Nathanial L. McConnell (FluxAugur) <fluxaugur@gmail.com>
2012-12-21 10:51:40 -0500

    Upload

---
+++ Proximity2/Classes/Controllers/AppController.h
@@ -0,0 +1,13 @@
+//
+//  AppController.h
+//  Proximity2
+//
+//  Created by Nathanial McConnell on 7/9/12.
+//  Copyright (c) 2012 East Tennessee State University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppController : NSObject
+
+@end
---
+++ Proximity2/Classes/Controllers/AppController.m
@@ -0,0 +1,13 @@
+//
+//  AppController.m
+//  Proximity2
+//
+//  Created by Nathanial McConnell on 7/9/12.
+//  Copyright (c) 2012 East Tennessee State University. All rights reserved.
+//
+
+#import "AppController.h"
+
+@implementation AppController
+
+@end
